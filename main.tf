4acdc65d9b596c8bf0277b8ab3ea88f8c8be6edc
jwang639 <jwang639@ucr.edu>
2022-02-08 15:29:36 -0800

    rmCredentials

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "ubuntu" {
+resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
