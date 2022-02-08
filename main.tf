d649a2c5af9112b1803754cae9f7ef1e5976e10d
jwang639 <jwang639@ucr.edu>
2022-02-08 13:13:37 -0800

    test6

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "ubuntu" {
-  ami           = 01163e76c844a2129
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
   tags = {
